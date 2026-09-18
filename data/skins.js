6d53aacc955caec357b9229d67bc152f73aec889
AmberFoxTerry <amberfoxterry@gmail.com>
2026-09-18 15:58:58 +0200

    Add SKINS object with starter skin details

---
+++ data/skins.js
@@ -0,0 +1,15 @@
+const SKINS = {
+
+    starter: {
+        name: "Starter",
+        rarity: "Common",
+
+        hp: 1000,
+
+        regeneration: {
+            amount: 100,
+            delay: 3
+        }
+    }
+
+};
