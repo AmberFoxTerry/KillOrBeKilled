8435f98abb2ec1e99a0f98b5d8df521c62c0b7dc
AmberFoxTerry <amberfoxterry@gmail.com>
2026-09-18 15:58:14 +0200

    Add WEAPONS object with pistol details

---
+++ data/weapons.js
@@ -0,0 +1,20 @@
+const WEAPONS = {
+
+    pistol: {
+        name: "Pistol",
+        rarity: "Common",
+
+        damage: 150,
+
+        cooldown: 0.5,
+
+        bullets: 1,
+
+        spread: 0,
+
+        range: 5,
+
+        bulletSize: 0.25
+    }
+
+};
